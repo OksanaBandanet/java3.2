e2cb352bae45f34dbbde90044f33186952e28eea
Oxana Banda <oksana.bandanet@gmail.com>
2023-03-03 01:08:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ваш баланс равен:");
+        int balance = 100;
+        int refill = 1500;
+        int bonus = refill/100;
+        if (refill > 1000) {
+            System.out.println(balance + refill + bonus);
+        } else {
+            System.out.println(balance + refill);
+        }
+    }
+}
